9223b0aa1e5e3c493439f406687e5eb4167d0494
Dozetpoet <144175417+Dozetpoet@users.noreply.github.com>
2024-10-09 21:47:45 -0600

    Update ALU.v

--- practica4/src/ALU.v
+++ practica4/src/ALU.v
@@ -4,7 +4,7 @@ module MIPSALU (ALUctl, A, B, ALUOut, Zero);
   output reg [31:0] ALUOut;
   output Zero;
   assign Zero = (ALUOut==0); //Zero is true if ALUOut is 0
-  always @(ALUctl, A, B) begin //
+  always @(ALUctl, A, B) begin ///
     case(ALUctl)
       0: ALUOut <= A & B;
       1: ALUOut <= A | B;
